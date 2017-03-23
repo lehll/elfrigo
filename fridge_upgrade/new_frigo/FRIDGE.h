c3a8a9840aaff201d3bf9d1351d35b17280dee89
theddes <timo@noprotocol.nl>
2017-03-23 19:09:19 +0100

    removed more merge conflicts

--- fridge_upgrade/new_frigo/FRIDGE.h
+++ fridge_upgrade/new_frigo/FRIDGE.h
@@ -20,14 +20,11 @@ class FRIDGE {
     //    temperature
     float getCurrentTemp();
     void setCurrentTemp(int ct);
-<<<<<<< HEAD
     float readCurrentTemp();
     float getCurrentVoltage(float v, float r1, float r2);
-=======
     int readCurrentTemp();
     //    voltage
     int getCurrentVoltage(float vPow, float r1, float r2);
->>>>>>> d5f94ec27d5216749141a3eab0cfe8f6588ac354
     void setCurrentVoltage(int cv);
     void emitSound();
 };
